3ed13a342ba6509372d50b2aacd493dcaef8d294
nchudinov <nchudinov@dataarmor.ru>
2022-08-02 17:59:16 +0300

    resolve CyclicSort_448

---
+++ src/com/leetcode/problems/CyclicSort_448.java
@@ -0,0 +1,34 @@
+package com.leetcode.problems;
+
+import java.util.*;
+
+public class CyclicSort_448 {
+	public static void main(String[] args) {
+		int[] nums = new int[] {4,3,2,7,8,2,3,1};
+		System.out.println(findDisappearedNumbers(nums));
+	}
+
+	public static List<Integer> findDisappearedNumbers(int[] nums) {
+		//sort array
+		int i = 0;
+		while (i < nums.length) {
+			int pos = nums[i] - 1;
+			if (nums[i] != nums[pos]) {
+				int temp = nums[i];
+				nums[i] = nums[pos];
+				nums[pos] = temp;
+			} else
+				++i;
+		}
+		
+		List<Integer> returnedList = new ArrayList<>();
+
+		for (int j = 0; j < nums.length; j++) {
+			if (j + 1 != nums[j]) {
+				returnedList.add(j+1);
+			}
+		}
+		return returnedList;
+		
+	}
+}
