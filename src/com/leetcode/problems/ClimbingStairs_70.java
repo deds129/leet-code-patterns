024355956c8bfe48d5e0b1d3bf573c913e4c2f4d
deds129 <nik.chudinov129@mail.ru>
2023-05-08 13:44:03 +0300

    pass ClimbingStairs_70

---
+++ src/com/leetcode/problems/ClimbingStairs_70.java
@@ -0,0 +1,39 @@
+package com.leetcode.problems;
+
+// source: https://leetcode.com/problems/climbing-stairs/
+
+public class ClimbingStairs_70 {
+	
+	public static void main(String[] args) {
+		System.out.println(climbStairs(5));
+	}
+
+
+	private static int climbStairs(int stepsCount) {
+
+		if (stepsCount == 1)
+			return 1;
+		if (stepsCount == 2)
+			return 2;
+		
+		int n1 = 1;
+		int n2 = 2;
+		int result = 0;
+		for (int i = 3; i <= stepsCount; i++) {
+			result = n1 + n2;
+			n1 = n2;
+			n2 = result;
+		}
+		return result;
+	}
+	
+	private static int recursiveClimbStairs(int stepsCount) {
+
+		if  (stepsCount == 1)
+			return 1;
+		if (stepsCount == 2) {
+			return 2;
+		}
+		return climbStairs(stepsCount - 1) + climbStairs(stepsCount - 2);
+	}
+}
